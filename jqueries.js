fd4661bc02ed6c6e9d26e0a03f995447f8c50fb9
uwan0 <72899775+uwan0@users.noreply.github.com>
2020-10-20 03:06:25 +0900

    Update jqueries.js

--- jqueries.js
+++ jqueries.js
@@ -1,9 +1,12 @@
-$("#sbmbtn").click(function (e) {
+/*$("#sbmbtn").click(function (e) {
   $.ajaxSubmit({
       headers: {
         "Authorization": "Bearer "+ opener.gactn
       },
   $("#submitForm").submit();
   });
-});
+});*/
 
+$('#submitForm').on('submit', function () {
+    $(this).ajaxSubmit();
+});
