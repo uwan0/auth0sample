69eb09c88bea34c73e5bb71c852853ae1908ac4a
uwan0 <72899775+uwan0@users.noreply.github.com>
2020-10-20 03:32:18 +0900

    Update jqueries.js

--- jqueries.js
+++ jqueries.js
@@ -15,5 +15,5 @@ $('#submitForm').submit(function() {
     // submit the form 
     $(this).ajaxSubmit(); 
     // return false to prevent normal browser submit and page navigation 
-    return false; 
+    //return false; 
 });
